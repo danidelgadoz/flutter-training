a16ad53a22b97ac373a645e51667873580e6f42f
Daniel <dedd1993@gmail.com>
2019-08-25 18:47:15 -0500

    navigate to new page

--- components/lib/src/pages/home_page.dart
+++ components/lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:components/src/providers/menu_provider.dart';
 import 'package:components/src/utils/icono_string_util.dart';
+import 'package:components/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -20,20 +21,26 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((item) {
       final widgetTemp = ListTile(
         title: Text(item['texto']),
         leading: getIcon(item['icon']),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue)
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context) => AlertPage()
+          );
+          Navigator.push(context, route);
+        },
       );
 
       opciones..add(widgetTemp)
