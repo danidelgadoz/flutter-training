a7d7bf12e085de8542d38b73661bd79e6243533c
Daniel <dedd1993@gmail.com>
2019-08-26 23:42:40 -0500

    Widget - Alert

--- components/lib/src/pages/alert_page.dart
+++ components/lib/src/pages/alert_page.dart
@@ -7,12 +7,53 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostart alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.keyboard_arrow_up),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es eñ contenido de la caja de la alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
     );
   }
 }
