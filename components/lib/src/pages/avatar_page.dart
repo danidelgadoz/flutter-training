59f2d7fc9c87c259c5613f3ebae9a6067dc73a3c
Daniel <dedd1993@gmail.com>
2019-08-27 22:22:16 -0500

    Widget - CircleAvatar

--- components/lib/src/pages/avatar_page.dart
+++ components/lib/src/pages/avatar_page.dart
@@ -6,6 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://iconsgarden.com/cache/icon_256/icons/10-nguyendoan88/JueHDp/preview.png'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://i.ytimg.com/vi/7cuZ0J5OkbQ/maxresdefault.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration( milliseconds: 200 ),
+        ),
       ),
     );
   }
