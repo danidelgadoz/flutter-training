4079589dbb578f776d7f9952c087a4e0152e2d06
Daniel <dedd1993@gmail.com>
2019-08-28 22:06:04 -0500

    Email and password fields

--- components/lib/src/pages/input_page.dart
+++ components/lib/src/pages/input_page.dart
@@ -6,7 +6,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +19,10 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
           Divider(),
           _crearPersona()
         ],
@@ -40,17 +45,52 @@ class _InputPageState extends State<InputPage> {
         suffixIcon: Icon(Icons.accessibility),
         icon: Icon(Icons.account_circle)
       ),
-      onChanged: (value) {
-        setState(() {
-          _nombre = value;
-        });
-      },
+      onChanged: (value) => setState(() {
+        _nombre = value;
+      })
+    );
+  }
+
+  _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+      ),
+      onChanged: (value) => setState(() {
+        _email = value;
+      })
+    );
+  }
+
+  _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (value) => setState(() {
+        _email = value;
+      })
     );
   }
 
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
