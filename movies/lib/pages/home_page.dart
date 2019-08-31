951b4bc3b9dce525e5a0c350ca906f11946d0fce
Daniel <dedd1993@gmail.com>
2019-08-31 17:16:40 -0500

    Custom widget - CardSwiper

--- movies/lib/pages/home_page.dart
+++ movies/lib/pages/home_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:movies/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hello world!!!!'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(
+      peliculas: [1,2,3,4]
     );
   }
 }
